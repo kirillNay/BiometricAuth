ff420a9f1cf1effd8020acfab59c50ad2f953f0a
k.naydyuk <k.naydyuk@okko.tv>
2022-05-18 23:44:55 +0300

    Create demo app with using simple authentication

--- app/src/main/java/nay/kirill/biometricauthdemo/MainActivity.kt
+++ app/src/main/java/nay/kirill/biometricauthdemo/MainActivity.kt
@@ -2,11 +2,42 @@ package nay.kirill.biometricauthdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.core.view.isVisible
+import nay.kirill.beomitric_auth.api.BiometricStoreManager
+import nay.kirill.beomitric_auth.api.BiometricType
 
 class MainActivity : AppCompatActivity() {
+
+    private val biometricStoreManager by lazy(BiometricStoreManager.Companion::create)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupBiometricUI()
+    }
+
+    private fun setupBiometricUI() {
+        val biometricUnavailable = findViewById<View>(R.id.biometricUnavailableMessage)
+        val authenticateButton = findViewById<View>(R.id.authenticateButton)
+
+        val isBiometricAvailable = biometricStoreManager.isBiometricAvailable(context = this, biometricType = BiometricType.SECOND_CLASS)
+
+        biometricUnavailable.isVisible = !isBiometricAvailable
+        authenticateButton.isVisible = isBiometricAvailable
+
+        if (isBiometricAvailable) {
+            authenticateButton.setOnClickListener {
+                biometricStoreManager.authenticate(
+                    activity = this,
+                    onSuccess = Toast.makeText(this, "Authentication success!", Toast.LENGTH_LONG)::show,
+                    onFailed = { error ->
+                        Toast.makeText(this, "Authentication failed with error: ${error.message}", Toast.LENGTH_LONG).show()
+                    }
+                )
+            }
+        }
     }
 }
