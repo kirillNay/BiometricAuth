90ee1edb708ca7f0d41280936ba08273032c5987
Kirill Nayduik <kirill.nayduikkn1@gmail.com>
2022-05-19 02:03:36 +0300

    Update demo app with new features

--- app/src/main/java/nay/kirill/biometricauthdemo/MainActivity.kt
+++ app/src/main/java/nay/kirill/biometricauthdemo/MainActivity.kt
@@ -3,6 +3,7 @@ package nay.kirill.biometricauthdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 import androidx.core.view.isVisible
 import nay.kirill.beomitric_auth.api.BiometricStoreManager
@@ -21,20 +22,51 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupBiometricUI() {
         val biometricUnavailable = findViewById<View>(R.id.biometricUnavailableMessage)
+
         val authenticateButton = findViewById<View>(R.id.authenticateButton)
+        val encryptButton = findViewById<View>(R.id.encryptButton)
+        val decryptButton = findViewById<View>(R.id.decryptButton)
+
+        val textToEncryptView = findViewById<EditText>(R.id.textToEncrypt)
 
         val isBiometricAvailable = biometricStoreManager.isBiometricAvailable(context = this, biometricType = BiometricType.SECOND_CLASS)
 
         biometricUnavailable.isVisible = !isBiometricAvailable
         authenticateButton.isVisible = isBiometricAvailable
+        encryptButton.isVisible = isBiometricAvailable
+        decryptButton.isVisible = isBiometricAvailable
+        textToEncryptView.isVisible = isBiometricAvailable
 
         if (isBiometricAvailable) {
             authenticateButton.setOnClickListener {
                 biometricStoreManager.authenticate(
                     activity = this,
-                    onSuccess = Toast.makeText(this, "Authentication success!", Toast.LENGTH_LONG)::show,
+                    onSuccess = Toast.makeText(this, "Аутентификация успешна!", Toast.LENGTH_LONG)::show,
                     onFailed = { error ->
-                        Toast.makeText(this, "Authentication failed with error: ${error.message}", Toast.LENGTH_LONG).show()
+                        Toast.makeText(this, "Аутентификация завершилась с ошибкой ${error.message}", Toast.LENGTH_LONG).show()
+                    }
+                )
+            }
+
+            encryptButton.setOnClickListener {
+                biometricStoreManager.encryptWithBiometric(
+                    activity = this,
+                    text = textToEncryptView.text.toString(),
+                    onSuccess = Toast.makeText(this, "Текст зашифрован", Toast.LENGTH_LONG)::show,
+                    onFailed = { error ->
+                        Toast.makeText(this, "Не удалось зашифровать текст. ${error.message}", Toast.LENGTH_LONG).show()
+                    }
+                )
+            }
+
+            decryptButton.setOnClickListener {
+                biometricStoreManager.decryptWithBiometric(
+                    activity = this,
+                    onSuccess = { text ->
+                        Toast.makeText(this, "Текст расшифрован: $text", Toast.LENGTH_LONG).show()
+                    },
+                    onFailed = { error ->
+                        Toast.makeText(this, "Не удалось расшифровать текст. ${error.message}", Toast.LENGTH_LONG).show()
                     }
                 )
             }
