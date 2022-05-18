e7170aec182c59c0b6d6503940ec48c2b107b122
k.naydyuk <k.naydyuk@okko.tv>
2022-05-18 23:04:27 +0300

    Create simple not crypto based authentication logic

--- app/beomitric_auth/src/main/java/nay/kirill/beomitric_auth/BiometricStoreManager.kt
+++ app/beomitric_auth/src/main/java/nay/kirill/beomitric_auth/BiometricStoreManager.kt
@@ -1,6 +1,8 @@
 package nay.kirill.beomitric_auth
 
+import android.app.Activity
 import android.content.Context
+import androidx.fragment.app.FragmentActivity
 
 /**
  * Provides API for using biometric authentication capabilities
@@ -9,10 +11,26 @@ interface BiometricStoreManager {
 
     /**
      * Checks if biometric scanners are ready to use and user registered biometric data
+     * @param context is Context of current Activity or Application
+     * @param biometricType is the class of biometric system to check availability of
      */
     fun isBiometricAvailable(
         context: Context,
         biometricType: BiometricType = BiometricType.FIRST_CLASS
     ): Boolean
+
+    /**
+     * Used to authenticate user using not crypto-base biometric system
+     */
+    fun authenticate(
+        activity: FragmentActivity,
+        onSuccess: () -> Unit,
+        onFailed: (error: AuthError) -> Unit
+    )
+
+    data class AuthError(
+        val errorCode: Int,
+        val message: String
+    )
 
 }
--- app/beomitric_auth/src/main/java/nay/kirill/beomitric_auth/BiometricStoreManagerImpl.kt
+++ app/beomitric_auth/src/main/java/nay/kirill/beomitric_auth/BiometricStoreManagerImpl.kt
@@ -2,6 +2,10 @@ package nay.kirill.beomitric_auth
 
 import android.content.Context
 import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentActivity
+import javax.crypto.Cipher
 
 class BiometricStoreManagerImpl : BiometricStoreManager {
 
@@ -9,5 +13,48 @@ class BiometricStoreManagerImpl : BiometricStoreManager {
         context: Context,
         biometricType: BiometricType
     ): Boolean = BiometricManager.from(context).canAuthenticate(biometricType.authenticator) == BiometricManager.BIOMETRIC_SUCCESS
+
+    override fun authenticate(
+        activity: FragmentActivity,
+        onSuccess: () -> Unit,
+        onFailed: (error: BiometricStoreManager.AuthError) -> Unit
+    ) {
+        val promptInfo = createPromptInfo(activity, biometricType = BiometricType.SECOND_CLASS)
+        val biometricPrompt = createBiometricPrompt(activity, onSuccess = { onSuccess.invoke() }, onFailed)
+        biometricPrompt.authenticate(promptInfo)
+    }
+
+    private fun createBiometricPrompt(
+        activity: FragmentActivity,
+        onSuccess: (Cipher) -> Unit,
+        onFailed: (error: BiometricStoreManager.AuthError) -> Unit
+    ): BiometricPrompt {
+        val executor = ContextCompat.getMainExecutor(activity)
+
+        val callback = object : BiometricPrompt.AuthenticationCallback() {
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                result.cryptoObject?.cipher?.let(onSuccess)
+            }
+
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                onFailed.invoke(BiometricStoreManager.AuthError(errorCode = errorCode, message = errString.toString()))
+            }
+        }
+
+        return BiometricPrompt(activity, executor, callback)
+    }
+
+    private fun createPromptInfo(
+        context: Context,
+        biometricType: BiometricType
+    ): BiometricPrompt.PromptInfo = BiometricPrompt.PromptInfo.Builder()
+        .apply {
+            setTitle(context.getString(R.string.android_biometric_dialog_title))
+            setDescription(context.getString(R.string.android_biometric_dialog_description))
+            setConfirmationRequired(false)
+            setNegativeButtonText(context.getString(R.string.android_biometric_dialog_negative_button))
+            setAllowedAuthenticators(biometricType.authenticator)
+        }
+        .build()
 
 }
